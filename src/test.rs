d4c78b320b034a250199b0c699496aaf532c4b6e
Niko Matsakis <niko@alum.mit.edu>
2014-10-02 12:00:52 -0400

    Code up quicksort example

--- src/test.rs
+++ src/test.rs
@@ -14,6 +14,56 @@ fn use_it() {
     assert_eq!(right, 44);
 }
 
+#[cfg(test)]
+fn quicksort(v: &mut [int]) {
+    if v.len() <= 1 {
+        return;
+    }
+
+    let pivot_value = v[0]; // simplest possible thing...
+    let mid = partition(pivot_value, v);
+    let (left, right) = v.mut_split_at(mid);
+    execute(&mut [
+        || quicksort(left),
+        || quicksort(right)
+    ]);
+
+    fn partition(pivot_value: int,
+                 v: &mut [int])
+                 -> uint
+    {
+        // Invariant:
+        //     .. l ==> less than or equal to pivot
+        //     r .. ==> greater than pivot
+        let mut l = 0;
+        let mut r = v.len() - 1;
+        while l <= r {
+            if v[l] > pivot_value {
+                v.swap(l, r);
+                r -= 1;
+            } else if v[r] <= pivot_value {
+                v.swap(l, r);
+                l += 1;
+            } else {
+                l += 1;
+                r -= 1;
+            }
+        }
+        return l;
+    }
+}
+
+#[test]
+fn call_quicksort() {
+    let mut v = [55, 12, 86, 8, 3, 5];
+    quicksort(v.as_mut_slice());
+    let mut bound = 0;
+    for &elem in v.iter() {
+        assert!(elem >= bound);
+        bound = elem;
+    }
+}
+
 // #[test]
 // fn use_it_bad() {
 //     let mut left: int = 0;
