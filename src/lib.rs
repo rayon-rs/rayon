bfadbb5a2f9ed87065434de9b9ea3e5fc0f61c5d
Niko Matsakis <niko@alum.mit.edu>
2015-12-04 20:46:26 -0500

    remove use of bencher

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![allow(non_camel_case_types)] // I prefer to use ALL_CAPS for type parameters
-#![cfg_attr(test, feature(test))]
 
 extern crate rand;
-
-#[cfg(test)]
-extern crate test as test_crate;
 
 #[macro_use]
 mod log;
--- src/test.rs
+++ src/test.rs
@@ -2,7 +2,6 @@
 
 use api::*;
 use rand::{Rng, SeedableRng, XorShiftRng};
-use test_crate::Bencher;
 
 fn quick_sort<T:PartialOrd+Send>(v: &mut [T]) {
     if v.len() <= 1 {
@@ -28,16 +27,14 @@ fn partition<T:PartialOrd+Send>(v: &mut [T]) -> usize {
     i
 }
 
-#[bench]
-fn sort(b: &mut Bencher) {
+#[test]
+fn sort() {
     let mut rng = XorShiftRng::from_seed([0, 1, 2, 3]);
     let mut data: Vec<_> = (0..6*1024).map(|_| rng.next_u32()).collect();
 
     initialize();
 
-    b.iter(|| {
-        quick_sort(&mut data);
-    });
+    quick_sort(&mut data);
 
     let mut sorted_data = data.clone();
     sorted_data.sort();
