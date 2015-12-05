b391a361478498ccdd1096958b292510a1508191
Niko Matsakis <niko@alum.mit.edu>
2015-12-05 04:57:29 -0500

    improve demo

--- demo/quicksort/src/main.rs
+++ demo/quicksort/src/main.rs
@@ -5,6 +5,8 @@ extern crate rayon;
 extern crate time;
 
 use rand::{Rng, SeedableRng, XorShiftRng};
+use std::env;
+use std::str::FromStr;
 
 trait Joiner {
     fn join<A,R_A,B,R_B>(oper_a: A, oper_b: B) -> (R_A, R_B)
@@ -56,26 +58,53 @@ fn partition<T:PartialOrd+Send>(v: &mut [T]) -> usize {
 }
 
 fn main() {
+    let mut kilo = 24;
+    let mut run_par = true;
+    let mut run_seq = true;
+
+    let args = env::args();
+    for arg in args.skip(1) {
+        if arg == "--no-par" {
+            run_par = false;
+        } else if arg == "--no-seq" {
+            run_seq = false;
+        } else {
+            match usize::from_str(&arg) {
+                Ok(v) => kilo = v,
+                Err(_) => {
+                    println!("invalid argument `{}`, expected integer", arg);
+                }
+            }
+        }
+    }
+
     let mut rng = XorShiftRng::from_seed([0, 1, 2, 3]);
-    let data: Vec<_> = (0..18*1024).map(|_| rng.next_u32()).collect();
+    let data: Vec<_> = (0..kilo*1024).map(|_| rng.next_u32()).collect();
 
     rayon::initialize();
 
-    let par_time = {
+    let par_time = if run_par {
         let mut data = data.clone();
         let par_start = time::precise_time_ns();
         quick_sort::<Parallel, u32>(&mut data);
         time::precise_time_ns() - par_start
+    } else {
+        0
     };
 
-    let seq_time = {
-        //let mut data = data.clone();
+    let seq_time = if run_seq {
+        let mut data = data.clone();
         let seq_start = time::precise_time_ns();
-        //quick_sort::<Sequential, u32>(&mut data);
+        quick_sort::<Sequential, u32>(&mut data);
         time::precise_time_ns() - seq_start
+    } else {
+        0
     };
 
-    println!("Parallel time   : {}", par_time);
-    println!("Sequential time : {}", seq_time);
-    println!("Parallel speedup: {}", (seq_time as f64) / (par_time as f64));
+    println!("Array length    : {}K 32-bit integers", kilo);
+    if run_par { println!("Parallel time   : {}", par_time); }
+    if run_seq { println!("Sequential time : {}", seq_time); }
+    if run_par && run_seq {
+        println!("Parallel speedup: {}", (seq_time as f64) / (par_time as f64));
+    }
 }
