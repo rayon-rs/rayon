68a52f4a8b3a7214aae72ff0578af59fb43dc877
Niko Matsakis <niko@alum.mit.edu>
2015-12-12 20:33:46 -0500

    wip

--- src/par_iter/mod.rs
+++ src/par_iter/mod.rs
@@ -2,9 +2,8 @@
 
 use api::join;
 use std::isize;
-use std::iter;
+use std::marker::PhantomData;
 use std::ops::Fn;
-use std::slice;
 use std::ptr;
 
 #[cfg(test)]
@@ -19,14 +18,15 @@ pub trait IntoParallelIterator {
 
 pub trait ParallelIterator {
     type Item;
-    type State: ParallelIteratorState<Item=Self::Item>;
+    type Shared: Sync;
+    type State: ParallelIteratorState<Shared=Self::Shared, Item=Self::Item>;
 
-    fn into_state(self) -> Self::State;
+    fn state(self) -> (Self::Shared, Self::State);
 
-    fn map<'map,OP,R>(self, op: &'map OP) -> Map<'map, Self, OP>
+    fn map<OP,R>(self, op: OP) -> Map<Self, OP>
         where OP: Fn(Self::Item) -> R, Self: Sized
     {
-        Map { base: self,  op: op }
+        Map { base: self,  map_op: op }
     }
 
     fn collect_into(self, target: &mut Vec<Self::Item>)
@@ -38,23 +38,24 @@ pub trait ParallelIterator {
 
 pub trait ParallelIteratorState: Sized {
     type Item;
-    type Iter: Iterator<Item=Self::Item>;
+    type Shared: Sync;
 
     fn len(&mut self) -> ParallelLen;
 
     fn split_at(self, index: usize) -> (Self, Self);
 
-    fn into_iter(self) -> Self::Iter;
+    fn for_each<OP>(self, shared: &Self::Shared, op: OP)
+        where OP: FnMut(Self::Item);
 
     /// Initializes some number of consecutive entries at
     /// `target`. Returns number that were initialized.
-    unsafe fn initialize(self, mut target: *mut Self::Item) -> usize {
+    unsafe fn initialize(self, shared: &Self::Shared, mut target: *mut Self::Item) -> usize {
         let mut counter = 0;
-        for item in self.into_iter() {
+        self.for_each(shared, |item| {
             ptr::write(target, item);
             target = target.offset(1);
             counter += 1;
-        }
+        });
         counter
     }
 }
@@ -140,16 +141,17 @@ impl<'map, T> IntoParallelIterator for &'map [T] {
 
 impl<'map, T> ParallelIterator for SliceIter<'map, T> {
     type Item = &'map T;
+    type Shared = ();
     type State = Self;
 
-    fn into_state(self) -> Self::State {
-        self
+    fn state(self) -> (Self::Shared, Self::State) {
+        ((), self)
     }
 }
 
 impl<'map, T> ParallelIteratorState for SliceIter<'map, T> {
     type Item = &'map T;
-    type Iter = slice::Iter<'map, T>;
+    type Shared = ();
 
     fn len(&mut self) -> ParallelLen {
         ParallelLen {
@@ -164,42 +166,57 @@ impl<'map, T> ParallelIteratorState for SliceIter<'map, T> {
         (left.into_par_iter(), right.into_par_iter())
     }
 
-    fn into_iter(self) -> Self::Iter {
-        self.slice.iter()
+    fn for_each<OP>(self, _shared: &Self::Shared, mut op: OP)
+        where OP: FnMut(&'map T)
+    {
+        for item in self.slice {
+            op(item);
+        }
     }
 }
 
 ///////////////////////////////////////////////////////////////////////////
 
-pub struct Map<'map, M, OP: 'map> {
+pub struct Map<M, MAP_OP> {
     base: M,
-    op: &'map OP,
+    map_op: MAP_OP,
 }
 
-impl<'map, M, OP, R> ParallelIterator for Map<'map, M, OP>
-    where M: ParallelIterator, OP: Fn(M::Item) -> R
+impl<M, MAP_OP, R> ParallelIterator for Map<M, MAP_OP>
+    where M: ParallelIterator,
+          MAP_OP: Fn(M::Item) -> R + Sync
 {
     type Item = R;
-    type State = MapState<'map, M::State, OP>;
+    type Shared = MapShared<M, MAP_OP>;
+    type State = MapState<M, MAP_OP>;
 
-    fn into_state(self) -> Self::State {
-        MapState {
-            base: self.base.into_state(),
-            op: self.op,
-        }
+    fn state(self) -> (Self::Shared, Self::State) {
+        let (base_shared, base_state) = self.base.state();
+        (MapShared { base: base_shared, map_op: self.map_op },
+         MapState { base: base_state, map_op: PhantomData })
     }
 }
 
-pub struct MapState<'map, M, OP: 'map> {
-    base: M,
-    op: &'map OP,
+pub struct MapShared<M, MAP_OP>
+    where M: ParallelIterator
+{
+    base: M::Shared,
+    map_op: MAP_OP
 }
 
-impl<'map, M, OP, R> ParallelIteratorState for MapState<'map, M, OP>
-    where M: ParallelIteratorState, OP: Fn(M::Item) -> R
+pub struct MapState<M, MAP_OP>
+    where M: ParallelIterator
 {
-    type Item = OP::Output;
-    type Iter = iter::Map<M::Iter, &'map OP>;
+    base: M::State,
+    map_op: PhantomData<MAP_OP>
+}
+
+impl<M, MAP_OP, R> ParallelIteratorState for MapState<M, MAP_OP>
+    where M: ParallelIterator,
+          MAP_OP: Fn(M::Item) -> R + Sync
+{
+    type Item = R;
+    type Shared = MapShared<M, MAP_OP>;
 
     fn len(&mut self) -> ParallelLen {
         self.base.len()
@@ -207,12 +224,16 @@ impl<'map, M, OP, R> ParallelIteratorState for MapState<'map, M, OP>
 
     fn split_at(self, index: usize) -> (Self, Self) {
         let (left, right) = self.base.split_at(index);
-        (MapState { base: left, op: self.op },
-         MapState { base: right, op: self.op })
+        (MapState { base: left, map_op: self.map_op },
+         MapState { base: right, map_op: self.map_op })
     }
 
-    fn into_iter(self) -> Self::Iter {
-        self.base.into_iter().map(self.op)
+    fn for_each<F>(self, shared: &Self::Shared, mut op: F)
+        where F: FnMut(R)
+    {
+        self.base.for_each(&shared.base, |item| {
+            op((shared.map_op)(item));
+        });
     }
 }
 
@@ -221,7 +242,7 @@ impl<'map, M, OP, R> ParallelIteratorState for MapState<'map, M, OP>
 pub fn collect_into<PAR_ITER,T>(pi: PAR_ITER, v: &mut Vec<T>)
     where PAR_ITER: ParallelIterator<Item=T>, PAR_ITER::State: Send
 {
-    let mut state = pi.into_state();
+    let (shared, mut state) = pi.state();
     let len = state.len();
 
     v.truncate(0); // clear any old data
@@ -229,23 +250,26 @@ pub fn collect_into<PAR_ITER,T>(pi: PAR_ITER, v: &mut Vec<T>)
     let target = v.as_mut_ptr(); // get a raw ptr
 
     unsafe {
-        collect_into_helper_with_len(state, len, CollectTarget(target));
+        collect_into_helper_with_len(state, &shared, len, CollectTarget(target));
     }
 
     unsafe {
+        // TODO -- drops are not quite right here!
         v.set_len(len.maximal_len);
     }
 }
 
 unsafe fn collect_into_helper<STATE,T>(mut state: STATE,
+                                       shared: &STATE::Shared,
                                        target: CollectTarget<T>)
     where STATE: ParallelIteratorState<Item=T> + Send
 {
     let len = state.len();
-    collect_into_helper_with_len(state, len, target)
+    collect_into_helper_with_len(state, shared, len, target)
 }
 
 unsafe fn collect_into_helper_with_len<STATE,T>(state: STATE,
+                                                shared: &STATE::Shared,
                                                 len: ParallelLen,
                                                 target: CollectTarget<T>)
     where STATE: ParallelIteratorState<Item=T> + Send
@@ -254,10 +278,10 @@ unsafe fn collect_into_helper_with_len<STATE,T>(state: STATE,
         let mid = len.maximal_len / 2;
         let (left, right) = state.split_at(mid);
         let (left_target, right_target) = target.split_at(mid);
-        join(|| collect_into_helper(left, left_target),
-             || collect_into_helper(right, right_target));
+        join(|| collect_into_helper(left, shared, left_target),
+             || collect_into_helper(right, shared, right_target));
     } else {
-        let initialized = state.initialize(target.as_mut_ptr());
+        let initialized = state.initialize(&shared, target.as_mut_ptr());
         assert_eq!(initialized, len.maximal_len);
     }
 }
@@ -279,13 +303,29 @@ impl<T> CollectTarget<T> {
 }
 
 ///////////////////////////////////////////////////////////////////////////
-// 
-// pub fn reduce<PAR_ITER,OP,T>(pi: PAR_ITER) -> T
-//     where PAR_ITER: ParallelIterator<Item=T>,
-//           PAR_ITER::State: Send,
-//           OP: Fn(T, T) -> T,
-// {
-//     let mut state = pi.into_state();
-//     let len = state.len();
-//     
-// }
+//
+//pub fn reduce<PAR_ITER,OP,T>(pi: PAR_ITER, op: OP) -> T
+//    where PAR_ITER: ParallelIterator<Item=T>,
+//          PAR_ITER::State: Send,
+//          OP: Fn(T, T) -> T,
+//{
+//    let mut state = pi.into_state();
+//    let len = state.len();
+//    reduce_helper_with_len(state, len)
+//}
+//
+//unsafe fn reduce_helper_with_len<STATE,T>(state: STATE,
+//                                          len: ParallelLen)
+//    where STATE: ParallelIteratorState<Item=T> + Send
+//{
+//    if len.cost > THRESHOLD && len.maximal_len > 1 {
+//        let mid = len.maximal_len / 2;
+//        let (left, right) = state.split_at(mid);
+//        let (left_target, right_target) = target.split_at(mid);
+//        join(|| collect_into_helper(left, left_target),
+//             || collect_into_helper(right, right_target));
+//    } else {
+//        let initialized = state.initialize(target.as_mut_ptr());
+//        assert_eq!(initialized, len.maximal_len);
+//    }
+//}
--- src/par_iter/test.rs
+++ src/par_iter/test.rs
@@ -5,7 +5,7 @@ pub fn execute() {
     let a: Vec<i32> = (0..1024).collect();
     let mut b = vec![];
     a.into_par_iter()
-     .map(&|&i| i + 1)
+     .map(|&i| i + 1)
      .collect_into(&mut b);
     let c: Vec<i32> = (0..1024).map(|i| i+1).collect();
     assert_eq!(b, c);
