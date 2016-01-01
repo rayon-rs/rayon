54602e1e282b16926756eed13414596f7b1cc1e3
Niko Matsakis <niko@alum.mit.edu>
2016-01-01 04:49:20 -0500

    Differentiate between spin and lock latches

--- src/latch.rs
+++ src/latch.rs
@@ -1,34 +1,83 @@
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Mutex, Condvar};
 use std::thread;
+
+pub trait Latch {
+    fn probe(&self) -> bool;
+    fn set(&self);
+    fn wait(&self);
+}
 
 /// A Latch starts as false and eventually becomes true. You can block
 /// until it becomes true.
-pub struct Latch {
+pub struct SpinLatch {
     b: AtomicBool
 }
 
-impl Latch {
+impl SpinLatch {
     #[inline]
-    pub fn new() -> Latch {
-        Latch {
+    pub fn new() -> SpinLatch {
+        SpinLatch {
             b: AtomicBool::new(false)
         }
     }
+}
+
+impl Latch for SpinLatch {
+    /// Test if latch is set.
+    fn probe(&self) -> bool {
+        self.b.load(Ordering::SeqCst)
+    }
 
     /// Set the latch to true, releasing all threads who are waiting.
-    pub fn set(&self) {
+    fn set(&self) {
         self.b.store(true, Ordering::SeqCst);
     }
 
     /// Spin until latch is set. Use with caution.
-    pub fn wait(&self) {
+    fn wait(&self) {
         while !self.probe() {
             thread::yield_now();
         }
     }
+}
 
+/// A Latch starts as false and eventually becomes true. You can block
+/// until it becomes true.
+pub struct LockLatch {
+    b: AtomicBool,
+    m: Mutex<()>,
+    v: Condvar,
+}
+
+impl LockLatch {
+    #[inline]
+    pub fn new() -> LockLatch {
+        LockLatch {
+            b: AtomicBool::new(false),
+            m: Mutex::new(()),
+            v: Condvar::new(),
+        }
+    }
+}
+
+impl Latch for LockLatch {
     /// Test if latch is set.
-    pub fn probe(&self) -> bool {
+    fn probe(&self) -> bool {
         self.b.load(Ordering::SeqCst)
+    }
+
+    /// Set the latch to true, releasing all threads who are waiting.
+    fn set(&self) {
+        self.b.store(true, Ordering::SeqCst);
+        self.v.notify_all();
+    }
+
+    /// Spin until latch is set. Use with caution.
+    fn wait(&self) {
+        let mut guard = self.m.lock().unwrap();
+        while !self.probe() {
+            guard = self.v.wait(guard).unwrap();
+        }
     }
 }
