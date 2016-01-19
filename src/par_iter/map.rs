b7fa30855685a1e7bd87a4d9c1eac59eaf390784
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2016-01-20 10:24:15 +1100

    Rename PhantomData wrapper and add comment for clarity

--- src/par_iter/map.rs
+++ src/par_iter/map.rs
@@ -24,39 +24,38 @@ impl<M, MAP_OP, R> ParallelIterator for Map<M, MAP_OP>
     fn state(self) -> (Self::Shared, Self::State) {
         let (base_shared, base_state) = self.base.state();
         (MapShared { base: base_shared, map_op: self.map_op },
-         MapState { base: base_state, map_op: PhantomMap::new() })
+         MapState { base: base_state, map_op: PhantomMapOp::new() })
     }
 }
 
 pub struct MapShared<M, MAP_OP>
-    where M: ParallelIterator
+    where M: ParallelIterator,
 {
     base: M::Shared,
-    map_op: MAP_OP
+    map_op: MAP_OP,
 }
 
 pub struct MapState<M, MAP_OP>
-    where M: ParallelIterator
+    where M: ParallelIterator,
 {
     base: M::State,
-    map_op: PhantomMap<MAP_OP>
+    map_op: PhantomMapOp<MAP_OP>,
 }
 
-pub struct PhantomMap<MAP_OP> {
-    data: PhantomData<*const MAP_OP>
-}
+// Wrapper for `PhantomData` to allow `MapState` to impl `Send`
+struct PhantomMapOp<MAP_OP>(PhantomData<*const MAP_OP>);
 
-impl<MAP_OP> PhantomMap<MAP_OP> {
-    fn new() -> PhantomMap<MAP_OP> {
-        PhantomMap { data: PhantomData }
+impl<MAP_OP> PhantomMapOp<MAP_OP> {
+    fn new() -> PhantomMapOp<MAP_OP> {
+        PhantomMapOp(PhantomData)
     }
 }
 
-unsafe impl<MAP_OP: Sync> Send for PhantomMap<MAP_OP> { }
+unsafe impl<MAP_OP: Sync> Send for PhantomMapOp<MAP_OP> { }
 
 impl<M, MAP_OP, R> ParallelIteratorState for MapState<M, MAP_OP>
     where M: ParallelIterator,
-          MAP_OP: Fn(M::Item) -> R + Sync
+          MAP_OP: Fn(M::Item) -> R + Sync,
 {
     type Item = R;
     type Shared = MapShared<M, MAP_OP>;
@@ -67,8 +66,8 @@ impl<M, MAP_OP, R> ParallelIteratorState for MapState<M, MAP_OP>
 
     fn split_at(self, index: usize) -> (Self, Self) {
         let (left, right) = self.base.split_at(index);
-        (MapState { base: left, map_op: PhantomMap::new() },
-         MapState { base: right, map_op: PhantomMap::new() })
+        (MapState { base: left, map_op: PhantomMapOp::new() },
+         MapState { base: right, map_op: PhantomMapOp::new() })
     }
 
     fn for_each<F>(self, shared: &Self::Shared, mut op: F)
