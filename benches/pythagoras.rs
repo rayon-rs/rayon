a73114153cbac2446d7928f62a5dcc71d4f3b314
Josh Stone <cuviper@gmail.com>
2016-01-29 01:24:54 -0800

    Add a benchmark counting Pythagorean triples
    This is inspired by problems I've solved for Project Euler, but I don't
    think it crosses any of those exactly.  I don't want to accidentally
    spoil any of their solutions!
    
    test euclid_default        ... bench:   9,851,407 ns/iter (+/- 151,222)
    test euclid_faux_serial    ... bench:   9,864,853 ns/iter (+/- 902,551)
    test euclid_parallel_full  ... bench:   6,906,225 ns/iter (+/- 2,883,072)
    test euclid_parallel_outer ... bench:   2,760,326 ns/iter (+/- 2,692,167)
    test euclid_serial         ... bench:   7,629,194 ns/iter (+/- 138,904)

---
+++ benches/pythagoras.rs
@@ -0,0 +1,79 @@
+//! How many Pythagorean triples exist less than or equal to a million?
+//! i.e. a²+b²=c² and a,b,c ≤ 1000000
+
+#![feature(test)]
+
+extern crate num;
+extern crate rayon;
+extern crate test;
+
+use num::Integer;
+use rayon::par_iter::*;
+use std::f64::INFINITY;
+use std::ops::Add;
+
+/// Use Euclid's formula to count Pythagorean triples
+///
+/// https://en.wikipedia.org/wiki/Pythagorean_triple#Generating_a_triple
+///
+/// For coprime integers m and n, with m > n and m-n is odd, then
+///     a = m²-n², b = 2mn, c = m²+n²
+///
+/// This is a coprime triple.  Multiplying by factors k covers all triples.
+fn par_euclid(m_weight: f64, n_weight: f64) -> u32 {
+    (1u32 .. 1000).into_par_iter().weight(m_weight).map(|m| {
+        (1 .. m).into_par_iter().weight(n_weight)
+            .filter(|n| (m - n).is_odd() && m.gcd(n) == 1)
+            .map(|n| 1000000 / (m*m + n*n))
+            .sum()
+    }).sum()
+}
+
+/// Same as par_euclid, without using rayon.
+fn euclid() -> u32 {
+    (1u32 .. 1000).map(|m| {
+        (1 .. m)
+            .filter(|n| (m - n).is_odd() && m.gcd(n) == 1)
+            .map(|n| 1000000 / (m*m + n*n))
+            .fold(0, Add::add)
+    }).fold(0, Add::add)
+}
+
+#[bench]
+/// Benchmark without rayon at all
+fn euclid_serial(b: &mut test::Bencher) {
+    let count = euclid();
+    b.iter(|| assert_eq!(euclid(), count))
+}
+
+#[bench]
+/// Use zero weights to force it fully serialized.
+fn euclid_faux_serial(b: &mut test::Bencher) {
+    rayon::initialize();
+    let count = euclid();
+    b.iter(|| assert_eq!(par_euclid(0.0, 0.0), count))
+}
+
+#[bench]
+/// Use the default weights (1.0)
+fn euclid_default(b: &mut test::Bencher) {
+    rayon::initialize();
+    let count = euclid();
+    b.iter(|| assert_eq!(par_euclid(1.0, 1.0), count))
+}
+
+#[bench]
+/// Use infinite weight to force the outer loop parallelized.
+fn euclid_parallel_outer(b: &mut test::Bencher) {
+    rayon::initialize();
+    let count = euclid();
+    b.iter(|| assert_eq!(par_euclid(INFINITY, 1.0), count))
+}
+
+#[bench]
+/// Use infinite weights to force it fully parallelized.
+fn euclid_parallel_full(b: &mut test::Bencher) {
+    rayon::initialize();
+    let count = euclid();
+    b.iter(|| assert_eq!(par_euclid(INFINITY, INFINITY), count))
+}
