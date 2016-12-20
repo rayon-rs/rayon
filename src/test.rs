9b7d9dfc0c7de764b239255f41ddf84bac3af1a3
Niko Matsakis <niko@alum.mit.edu>
2016-12-20 15:56:09 -0500

    test thread index functions

--- src/test.rs
+++ src/test.rs
@@ -168,3 +168,12 @@ fn coerce_to_box_error() {
     // check that coercion succeeds
     let _: Box<Error> = From::from(InitError::NumberOfThreadsZero);
 }
+
+#[test]
+fn worker_thread_index() {
+    let pool = ThreadPool::new(Configuration::new().set_num_threads(22)).unwrap();
+    assert_eq!(pool.num_threads(), 22);
+    assert_eq!(pool.current_thread_index(), None);
+    let index = pool.install(|| pool.current_thread_index().unwrap());
+    assert!(index < 22);
+}
