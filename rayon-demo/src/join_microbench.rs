5d6d120b8d9ea28bfb00d31d9f4f32facfb26c66
Josh Stone <cuviper@gmail.com>
2017-03-14 21:35:02 -0700

    Add quick benchmarks of min/max len

--- rayon-demo/src/join_microbench.rs
+++ rayon-demo/src/join_microbench.rs
@@ -14,6 +14,26 @@ fn increment_all(b: &mut Bencher) {
 }
 
 #[bench]
+fn increment_all_min(b: &mut Bencher) {
+    let mut big_vec = vec![0_usize; 100*1024];
+    b.iter(|| {
+        big_vec.par_iter_mut()
+               .set_min_len(1024)
+               .for_each(|p| *p = p.wrapping_add(1));
+    });
+}
+
+#[bench]
+fn increment_all_max(b: &mut Bencher) {
+    let mut big_vec = vec![0_usize; 100*1024];
+    b.iter(|| {
+        big_vec.par_iter_mut()
+               .set_max_len(100)
+               .for_each(|p| *p = p.wrapping_add(1));
+    });
+}
+
+#[bench]
 fn join_recursively(b: &mut Bencher) {
     fn join_recursively(n: usize) {
         if n == 0 {
