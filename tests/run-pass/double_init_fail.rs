42accefdd16daf9f4e7d23539567380d7c32c89f
leonardo.yvens <leoyvens@gmail.com>
2017-03-17 18:52:46 -0300

    fix test

---
+++ tests/run-pass/double_init_fail.rs
@@ -0,0 +1,10 @@
+extern crate rayon;
+
+use rayon::*;
+
+fn main() {
+    let result1 = initialize(Configuration::new());
+    assert_eq!(result1, Ok(()));
+    let result2 = initialize(Configuration::new());
+    assert_eq!(result2, Err(InitError::GlobalPoolAlreadyInitialized));
+}
