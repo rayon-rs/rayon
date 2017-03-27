5cf0082f122666ca1d044c357a8ea3451793186c
Nathan Froyd <froydnj@gmail.com>
2017-03-27 13:21:16 -0400

    add a test for panic'ing {start,exit}_handlers

--- rayon-core/src/test.rs
+++ rayon-core/src/test.rs
@@ -71,3 +71,53 @@ fn exit_callback_called() {
     // The handler must have been called on every exiting thread.
     assert_eq!(n_called.load(Ordering::SeqCst), n_threads);
 }
+
+#[test]
+fn handler_panics_handled_correctly() {
+    let n_threads = 16;
+    let n_called = Arc::new(AtomicUsize::new(0));
+    // Wait for all the threads in the pool plus the one running tests.
+    let start_barrier = Arc::new(Barrier::new(n_threads + 1));
+    let exit_barrier = Arc::new(Barrier::new(n_threads + 1));
+
+    let start_handler: StartHandler = Arc::new(move |_| {
+        panic!("ensure panic handler is called when starting");
+    });
+    let exit_handler: ExitHandler = Arc::new(move |_| {
+        panic!("ensure panic handler is called when exiting");
+    });
+
+    let sb = start_barrier.clone();
+    let eb = exit_barrier.clone();
+    let nc = n_called.clone();
+    let panic_handler: PanicHandler = Arc::new(move |_| {
+        let val = nc.fetch_add(1, Ordering::SeqCst);
+        if val < n_threads {
+            sb.wait();
+        } else {
+            eb.wait();
+        }
+    });
+
+    let conf = Configuration::new()
+        .set_num_threads(n_threads)
+        .set_start_handler(start_handler)
+        .set_exit_handler(exit_handler)
+        .set_panic_handler(panic_handler);
+    {
+        let _ = ThreadPool::new(conf).unwrap();
+
+        // Wait for all the threads to start, panic in the start handler,
+        // and been taken care of by the panic handler.
+        start_barrier.wait();
+
+        // Drop the pool so it stops the running threads.
+    }
+
+    // Wait for all the threads to exit, panic in the exit handler,
+    // and been taken care of by the panic handler.
+    exit_barrier.wait();
+
+    // The panic handler must have been called twice on every thread.
+    assert_eq!(n_called.load(Ordering::SeqCst), 2 * n_threads);
+}
