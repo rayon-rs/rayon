20b89f5739d844859b930bc011b56b42fecaab6f
Josh Stone <cuviper@gmail.com>
2017-04-01 12:02:35 -0700

    Output build.rs rerun-if-changed

--- rayon-core/build.rs
+++ rayon-core/build.rs
@@ -2,4 +2,6 @@
 // *actually* linking to anything, just making sure that we're the only
 // rayon-core in use.
 fn main() {
+    // we don't need to rebuild for anything else
+    println!("cargo:rerun-if-changed=build.rs");
 }
