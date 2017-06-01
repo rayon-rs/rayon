3037fdac3909ff8c605c24a45ccc8b4100591a61
Christopher Davenport <Chris@ChristopherDavenport.tech>
2017-05-31 20:28:11 -0400

    Remove Repeat boxing, and make test use explicit
    Needs something for using the take method which is missing, although elt should be available for any element

--- src/iter/repeat.rs
+++ src/iter/repeat.rs
@@ -10,12 +10,6 @@ pub fn repeat<T: Clone>(elt: T) -> Repeat<T> {
     Repeat { element: elt }
 }
 
-impl<T: Clone> Clone for Repeat<T> {
-    fn clone(&self) -> Repeat<T> {
-        Repeat { element: self.element.clone() }
-    }
-}
-
 impl<T> ParallelIterator for Repeat<T>
     where T: Clone + Send
 {
@@ -24,7 +18,7 @@ impl<T> ParallelIterator for Repeat<T>
     fn drive_unindexed<C>(self, consumer: C) -> C::Result
         where C: UnindexedConsumer<Self::Item>
     {
-        bridge(self, consumer)
+        bridge_unindexed(self, consumer)
     }
 }
 
@@ -40,7 +34,7 @@ impl<T> IndexedParallelIterator for Repeat<T>
     fn with_producer<CB>(self, callback: CB) -> CB::Output
         where CB: ProducerCallback<Self::Item>
     {
-        callback.callback(RepeatProducer { repeat: self })
+        callback.callback(RepeatProducer { repeat: self.element })
     }
 
     fn len(&mut self) -> usize {
@@ -52,7 +46,7 @@ impl<T> IndexedParallelIterator for Repeat<T>
 /// Producer
 
 struct RepeatProducer<P> {
-    repeat: Repeat<P>,
+    repeat: P,
 }
 
 impl<T: Clone + Send> Producer for RepeatProducer<T> {
@@ -64,8 +58,7 @@ impl<T: Clone + Send> Producer for RepeatProducer<T> {
     }
 
     fn split_at(self, index: usize) -> (Self, Self) {
-        (RepeatProducer { repeat: self.repeat.clone() },
-         RepeatProducer { repeat: self.repeat.clone() })
+        (RepeatProducer { repeat: self.repeat.clone() }, RepeatProducer { repeat: self.repeat })
     }
 }
 
@@ -73,20 +66,20 @@ impl<T: Clone + Send> UnindexedProducer for Repeat<T> {
     type Item = T;
 
     fn split(self) -> (Self, Option<Self>) {
-        (Repeat { element: self.element.clone() }, Some(Repeat { element: self.element.clone() }))
+        (Repeat { element: self.element.clone() }, Some(Repeat { element: self.element }))
     }
 
     fn fold_with<F>(self, folder: F) -> F
         where F: Folder<T>
     {
-        folder.consume_iter(RepeatIter { repeat: Repeat { element: self.element } })
+        folder.consume_iter(RepeatIter { repeat: self.element })
     }
 }
 
 /// Repeat Iter Create As Repeat Does Not Have ExactSizeIterator
 
 struct RepeatIter<T> {
-    repeat: Repeat<T>,
+    repeat: T,
 }
 
 impl<T: Clone> Iterator for RepeatIter<T> {
@@ -94,7 +87,7 @@ impl<T: Clone> Iterator for RepeatIter<T> {
 
     #[inline]
     fn next(&mut self) -> Option<T> {
-        Some(self.repeat.element.clone())
+        Some(self.repeat.clone())
     }
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -105,7 +98,7 @@ impl<T: Clone> Iterator for RepeatIter<T> {
 impl<A: Clone> DoubleEndedIterator for RepeatIter<A> {
     #[inline]
     fn next_back(&mut self) -> Option<A> {
-        Some(self.repeat.element.clone())
+        Some(self.repeat.clone())
     }
 }
 
