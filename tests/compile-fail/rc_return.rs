8b71c865b2d66c581476e4e7b4870840ba691aa2
Josh Stone <cuviper@gmail.com>
2017-06-19 11:40:09 -0700

    Split up the test of join returning Rc
    It seems that current rustc is now only reporting one error for `Rc`
    returned in both `join` arguments.  We can split our test into two lines
    to explicitly check the failure in each argument position.

--- tests/compile-fail/rc_return.rs
+++ tests/compile-fail/rc_return.rs
@@ -3,7 +3,6 @@ extern crate rayon;
 use std::rc::Rc;
 
 fn main() {
-    rayon::join(|| Rc::new(22), || Rc::new(23));
-    //~^ ERROR E0277
-    //~| ERROR E0277
+    rayon::join(|| Rc::new(22), || ()); //~ ERROR E0277
+    rayon::join(|| (), || Rc::new(23)); //~ ERROR E0277
 }
