7a78709384acc45ead54051af49e8c4ca6fbc96a
Stjepan Glavina <stjepang@gmail.com>
2017-06-23 01:07:23 +0200

    Fix the build on Rust 1.12.0

--- src/slice/mergesort.rs
+++ src/slice/mergesort.rs
@@ -4,8 +4,9 @@
 //! The only difference from the original is that the sequential `mergesort` returns
 //! `MergesortResult` and leaves descending arrays intact.
 
+use iter::*;
 use rayon_core;
-use slice::*;
+use slice::ParallelSliceMut;
 use std::mem::size_of;
 use std::mem;
 use std::ptr;
