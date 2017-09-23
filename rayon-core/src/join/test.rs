502790c33567717dc7c2a6c142a34d45381f74b6
Josh Stone <cuviper@gmail.com>
2017-09-22 22:35:21 -0700

    Fix the `join_context` tests for `FnContext: !Send`

--- rayon-core/src/join/test.rs
+++ rayon-core/src/join/test.rs
@@ -81,18 +81,20 @@ fn panic_b_still_executes() {
 #[test]
 fn join_context_both() {
     // If we're not in a pool, both should be marked stolen as they're injected.
-    let (a, b) = join_context(|a| a, |b| b);
-    assert!(a.migrated());
-    assert!(b.migrated());
+    let (a_migrated, b_migrated) = join_context(|a| a.migrated(), |b| b.migrated());
+    assert!(a_migrated);
+    assert!(b_migrated);
 }
 
 #[test]
 fn join_context_neither() {
     // If we're already in a 1-thread pool, neither job should be stolen.
     let pool = ThreadPool::new(Configuration::new().num_threads(1)).unwrap();
-    let (a, b) = pool.install(|| join_context(|a| a, |b| b));
-    assert!(!a.migrated());
-    assert!(!b.migrated());
+    let (a_migrated, b_migrated) = pool.install(|| {
+        join_context(|a| a.migrated(), |b| b.migrated())
+    });
+    assert!(!a_migrated);
+    assert!(!b_migrated);
 }
 
 #[test]
@@ -102,8 +104,10 @@ fn join_context_second() {
     // If we're already in a 2-thread pool, the second job should be stolen.
     let barrier = Barrier::new(2);
     let pool = ThreadPool::new(Configuration::new().num_threads(2)).unwrap();
-    let (a, b) = pool.install(|| join_context(|a| { barrier.wait(); a },
-                                              |b| { barrier.wait(); b }));
-    assert!(!a.migrated());
-    assert!(b.migrated());
+    let (a_migrated, b_migrated) = pool.install(|| {
+        join_context(|a| { barrier.wait(); a.migrated() },
+                     |b| { barrier.wait(); b.migrated() })
+    });
+    assert!(!a_migrated);
+    assert!(b_migrated);
 }
