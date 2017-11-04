93d1256701a3a8960d2a5041c8a2c4edc78d9ff2
Kerollmops <renault.cle@gmail.com>
2017-11-04 18:20:46 +0100

    Add an example to the `min_by_key` method doc

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -716,9 +716,13 @@ pub trait ParallelIterator: Sized + Send {
     /// specified, so if the `Ord` impl is not truly associative, then
     /// the results are not deterministic.
     ///
+    /// # Examples
+    ///
     /// ```
     /// use rayon::prelude::*;
+    ///
     /// let a = [-3_i32, 34, 2, 5, -10, -3, -23];
+    ///
     /// assert_eq!(a.par_iter().min_by_key(|x| x.abs()), Some(&2));
     /// ```
     fn min_by_key<K, F>(self, f: F) -> Option<Self::Item>
