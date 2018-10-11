7222d9b314eda6ca19e143e77107b081db5253a5
Matthew Donoughe <mdonoughe@gmail.com>
2018-10-10 21:45:11 -0400

    use platform-specific dependencies

--- rayon-demo/src/main.rs
+++ rayon-demo/src/main.rs
@@ -33,6 +33,7 @@ extern crate serde; // all
 extern crate cgmath; // nbody
 #[macro_use]
 extern crate glium; // nbody
+#[cfg(unix)]
 extern crate libc; // life
 extern crate rand; // nbody
 extern crate time; // nbody, sieve
@@ -43,6 +44,7 @@ extern crate num; // factorial
 extern crate lazy_static; // find
 extern crate fixedbitset; // tsp
 extern crate regex; // tsp
+#[cfg(windows)]
 extern crate winapi; // life
 
 #[cfg(test)]
