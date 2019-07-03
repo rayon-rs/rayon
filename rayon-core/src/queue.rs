bb16630dfa1a0e2a00104af937eaca5f70338cc8
Ralf Biedert <rb@xr.io>
2019-07-03 09:48:27 +0200

    Queue, not the other ... thing.

--- rayon-core/src/queue.rs
+++ rayon-core/src/queue.rs
@@ -1,45 +1,49 @@
 use crossbeam_queue::{ArrayQueue, PopError, SegQueue};
 
 /// A data exchange structure that tries to be a queue and almost succeeds.
-///
-/// This structure is useful if you want a possibly unbounded queue, and are willing to trade
-/// strict ordering for predictable allocations up to a certain point. In particular, if you
-/// load fewer than `cap` elements, this structure will not allocate during `push()` or `pop()`.
-///
-/// Also, up to a load of `cap` the structure will behave like a regular, pre-allocated queue.
-/// Beyond that, the ordering of elements might be random until the load falls below `cap` again.
-pub(super) struct AlmostQueue<T> {
+pub(crate) struct AlmostQueue<T> {
     unbounded: SegQueue<T>,
     bounded: ArrayQueue<T>,
 }
 
 impl<T> AlmostQueue<T> {
-    pub(super) fn new(cap: usize) -> Self {
+    pub(crate) fn new(cap: usize) -> Self {
         Self {
             unbounded: SegQueue::new(),
             bounded: ArrayQueue::new(cap),
         }
     }
-
-    pub(super) fn push(&self, t: T) {
-        if let Err(x) = self.bounded.push(t) {
-            // At this point we know we failed to insert into the bounded queue. In the meantime
-            // (between the Err and the next line) the bounded one might have become empty again,
-            // but we insert into the unbounded one anyway.
-            self.unbounded.push(x.0)
+    
+    pub(crate) fn push(&self, t: T) {
+        // If unbounded has any elements, it always takes over receiving elements.
+        if !self.unbounded.is_empty() {
+            // Once we entered here, bounded might have become empty again, and a newer element
+            // already be on the way to the bounded queue. That newer element would move to the
+            // bounded queue, and be handled before our element we just entered.
+            self.unbounded.push(t);
+        } else {
+            // If we enter here, unbounded might have become inhabited again with a newer element.
+            // In that case we would append our older element to the bounded queue. However, that
+            // would be fine, since we always process the bounded queue first.
+            if let Err(e) = self.bounded.push(t) {
+                // In this pathological case, we wanted to have inserted our older element
+                // into bounded, but couldn't because another element was stealing our slot.
+                // In that case we flow over to unbounded again, and our order might be out of
+                // place until all the elements in between have been processed.
+                self.unbounded.push(e.0);
+            }
         }
     }
-
-    pub(super) fn pop(&self) -> Result<T, PopError> {
-        // We have to query the bounded one first, since it might contain overflow elements even if
-        // the bounded one is empty.
-        let unbounded = self.unbounded.pop();
-
-        if unbounded.is_ok() {
-            unbounded
+    
+    pub(crate) fn pop(&self) -> Result<T, PopError> {
+        // We always pop the bounded first. It is filled first when the queue is empty,
+        // and might spuriously be filled if popping happens during pushing.
+        let x = self.bounded.pop();
+        
+        if x.is_ok() {
+            x
         } else {
-            // We only return bounded elements if we are sure there are no unbounded elements left.
-            self.bounded.pop()
+            self.unbounded.pop()
         }
     }
 }
