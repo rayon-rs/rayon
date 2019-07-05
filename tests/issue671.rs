224287bff5eb7fc287be8087d09f646f32160f1f
Josh Stone <cuviper@gmail.com>
2019-07-05 12:01:03 -0700

    Reduce the genericity of WhileSomeFolder::consume_iter's closure
    The `take_while` closure only needs to be generic in `T`, along with a
    captured `&AtomicBool`. When we write the closure directly, it carries
    all the type baggage of `WhileSomeFolder<'f, C>::consumer_iter<I>`,
    where the monomorphized type may explode. Instead, we can move this
    closure to a standalone function for reduced genericity.

---
+++ tests/issue671.rs
@@ -0,0 +1,16 @@
+extern crate rayon;
+
+use rayon::prelude::*;
+
+#[test]
+fn type_length_limit() {
+    let _ = Vec::<Result<(), ()>>::new()
+        .into_par_iter()
+        .map(|x| x)
+        .map(|x| x)
+        .map(|x| x)
+        .map(|x| x)
+        .map(|x| x)
+        .map(|x| x)
+        .collect::<Result<(), ()>>();
+}
