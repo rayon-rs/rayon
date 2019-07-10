249fbb906d3cb292e57779f2dde9208db6b43fc1
Josh Stone <cuviper@gmail.com>
2019-07-10 13:56:54 -0700

    Reduce the genericity of extend closures
    Most of these closures only need to be generic in the item type, but
    when created in the context of some `I` generic iterator, they inherit
    that genericity. This affects both type length and code duplication.

---
+++ tests/issue671-unzip.rs
@@ -0,0 +1,19 @@
+#![type_length_limit = "10000"]
+
+extern crate rayon;
+
+use rayon::prelude::*;
+
+#[test]
+fn type_length_limit() {
+    let input = vec![1, 2, 3, 4, 5];
+    let (indexes, (squares, cubes)): (Vec<_>, (Vec<_>, Vec<_>)) = input
+        .par_iter()
+        .map(|x| (x * x, x * x * x))
+        .enumerate()
+        .unzip();
+
+    drop(indexes);
+    drop(squares);
+    drop(cubes);
+}
