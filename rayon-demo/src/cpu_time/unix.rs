9b4d9798def1ca954b8958fcd22b6a4088af3180
Josh Stone <cuviper@gmail.com>
2019-11-20 11:44:12 -0800

    Avoid mem::uninitialized in the demo cpu_time
    For both Unix `getrusage` and Windows `GetProcessTimes`, we can pass
    `MaybeUninit` pointers to those FFI calls and `assume_init()` afterward.

--- rayon-demo/src/cpu_time/unix.rs
+++ rayon-demo/src/cpu_time/unix.rs
@@ -1,14 +1,17 @@
-use libc::{getrusage, rusage, RUSAGE_SELF};
-use std::mem;
+use libc::{getrusage, RUSAGE_SELF};
+use std::mem::MaybeUninit;
 
 pub fn get_cpu_time() -> Option<u64> {
-    unsafe {
-        let mut usage: rusage = mem::uninitialized();
-        getrusage(RUSAGE_SELF, &mut usage);
-        let user = 1_000_000_000 * (usage.ru_utime.tv_sec as u64)
-            + 1_000 * (usage.ru_utime.tv_usec as u64);
-        let system = 1_000_000_000 * (usage.ru_stime.tv_sec as u64)
-            + 1_000 * (usage.ru_stime.tv_usec as u64);
-        Some(user + system)
-    }
+    let usage = unsafe {
+        let mut usage = MaybeUninit::uninit();
+        if getrusage(RUSAGE_SELF, usage.as_mut_ptr()) != 0 {
+            return None;
+        }
+        usage.assume_init()
+    };
+    let user =
+        1_000_000_000 * (usage.ru_utime.tv_sec as u64) + 1_000 * (usage.ru_utime.tv_usec as u64);
+    let system =
+        1_000_000_000 * (usage.ru_stime.tv_sec as u64) + 1_000 * (usage.ru_stime.tv_usec as u64);
+    Some(user + system)
 }
