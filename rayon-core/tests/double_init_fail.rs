c273ddcbdfe008131fa917bc8899c42e75cb528f
Josh Stone <cuviper@gmail.com>
2020-04-03 11:22:47 -0700

    Modernize Error for ThreadPoolBuildError
    - Add `source` to return the inner `io::Error`
    - `#[allow(deprecated)] on the vestigial `description`.

--- rayon-core/tests/double_init_fail.rs
+++ rayon-core/tests/double_init_fail.rs
@@ -6,5 +6,9 @@ fn double_init_fail() {
     let result1 = ThreadPoolBuilder::new().build_global();
     assert_eq!(result1.unwrap(), ());
     let err = ThreadPoolBuilder::new().build_global().unwrap_err();
-    assert!(err.description() == "The global thread pool has already been initialized.");
+    assert!(err.source().is_none());
+    assert_eq!(
+        err.to_string(),
+        "The global thread pool has already been initialized.",
+    );
 }
