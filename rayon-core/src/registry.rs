c7d963a9c242c915c4ed320fc79a0634d506ccd0
Josh Stone <cuviper@gmail.com>
2020-08-17 16:14:27 -0700

    Use crossbeam_deque::Injector instead of crossbeam_queue::SegQueue
    `Injector` and `SegQueue` are _almost_ identical, down to the very same
    comments in their implementations. One difference is that `Injector`
    allocates its first block as soon as it's created, but `SegQueue` waits
    until its first `push`, which complicates it to allow being `null`.
    `Injector` also has methods to steal batches into a deque `Worker`,
    which might be useful to us.
    
    At the very least, this lets us trim a dependency.

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -8,8 +8,7 @@ use crate::util::leak;
 use crate::{
     ErrorKind, ExitHandler, PanicHandler, StartHandler, ThreadPoolBuildError, ThreadPoolBuilder,
 };
-use crossbeam_deque::{Steal, Stealer, Worker};
-use crossbeam_queue::SegQueue;
+use crossbeam_deque::{Injector, Steal, Stealer, Worker};
 use std::any::Any;
 use std::cell::Cell;
 use std::collections::hash_map::DefaultHasher;
@@ -136,7 +135,7 @@ pub(super) struct Registry {
     logger: Logger,
     thread_infos: Vec<ThreadInfo>,
     sleep: Sleep,
-    injected_jobs: SegQueue<JobRef>,
+    injected_jobs: Injector<JobRef>,
     panic_handler: Option<Box<PanicHandler>>,
     start_handler: Option<Box<StartHandler>>,
     exit_handler: Option<Box<ExitHandler>>,
@@ -240,7 +239,7 @@ impl Registry {
             logger: logger.clone(),
             thread_infos: stealers.into_iter().map(ThreadInfo::new).collect(),
             sleep: Sleep::new(logger, n_threads),
-            injected_jobs: SegQueue::new(),
+            injected_jobs: Injector::new(),
             terminate_count: AtomicUsize::new(1),
             panic_handler: builder.take_panic_handler(),
             start_handler: builder.take_start_handler(),
@@ -413,13 +412,18 @@ impl Registry {
     }
 
     fn pop_injected_job(&self, worker_index: usize) -> Option<JobRef> {
-        let job = self.injected_jobs.pop().ok();
-        if job.is_some() {
-            self.log(|| JobUninjected {
-                worker: worker_index,
-            });
+        loop {
+            match self.injected_jobs.steal() {
+                Steal::Success(job) => {
+                    self.log(|| JobUninjected {
+                        worker: worker_index,
+                    });
+                    return Some(job);
+                }
+                Steal::Empty => return None,
+                Steal::Retry => {}
+            }
         }
-        job
     }
 
     /// If already in a worker-thread of this registry, just execute `op`.
