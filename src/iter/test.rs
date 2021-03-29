ff4056c9f54a919e9705cdfd453d6f92cc72081a
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2021-03-29 10:20:22 +0200

    tests for blocks operations

--- src/iter/test.rs
+++ src/iter/test.rs
@@ -2316,3 +2316,19 @@ fn walk_tree_postfix_degree5() {
     .collect();
     assert_eq!(v, nodes)
 }
+
+#[test]
+fn blocks() {
+    let count = AtomicUsize::new(0);
+    let v: Vec<usize> = (0..1000)
+        .into_par_iter()
+        .map(|_| count.fetch_add(1, Ordering::Relaxed))
+        .by_uniform_blocks(100)
+        .collect();
+    let m = v
+        .chunks(100)
+        .map(|c| c.iter().max().copied().unwrap())
+        .collect::<Vec<usize>>();
+    assert!(m.windows(2).all(|w| w[0].lt(&w[1])));
+    assert_eq!(v.len(), 1000);
+}
