1be2b130035013302656d8e398c3dd72a343ad8a
wagnerf42 <frederic.wagner@imag.fr>
2021-04-12 10:03:24 +0200

    Update src/iter/walk_tree.rs
    Co-authored-by: Niko Matsakis <niko@alum.mit.edu>

--- src/iter/walk_tree.rs
+++ src/iter/walk_tree.rs
@@ -111,7 +111,7 @@ where
 /// # Ordering
 ///
 /// This function guarantees a prefix ordering. See also [`walk_tree_postfix`], which guarantees a postfix order. If you don't care about ordering, you should use `walk_tree`, which will use whatever is believed to be fastest.
-/// Between siblings, last bred child comes first.
+/// Between siblings, children are reduced in reverse order -- that is, the children that returned last are reduced first.
 /// For example a perfect binary tree of 7 nodes will reduced in the following order:
 ///
 /// ```text
