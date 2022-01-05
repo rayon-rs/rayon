3a7f1a8716d7ca8fc4f9163542d7e599bd1df6f5
Ben Kimock <kimockb@gmail.com>
2022-01-04 20:26:12 -0500

    Ensure slice lifetime stays correct

--- src/vec.rs
+++ src/vec.rs
@@ -144,7 +144,7 @@ impl<'data, T: Send> IndexedParallelIterator for Drain<'data, T> {
             // Create the producer as the exclusive "owner" of the slice.
             let producer = {
                 let ptr = self.vec.as_mut_ptr().add(start);
-                let slice = slice::from_raw_parts_mut(ptr, self.range.len());
+                let slice: &'data mut [T] = slice::from_raw_parts_mut(ptr, self.range.len());
                 DrainProducer::new(slice)
             };
 
