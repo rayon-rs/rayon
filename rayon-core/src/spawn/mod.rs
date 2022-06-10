ae75d26c5f39a1417e0532797b0915a2bd01b5b0
Josh Stone <cuviper@gmail.com>
2022-06-09 17:42:06 -0700

    Remove `mem::transmute` calls from job handling
    Where `JobRef` was transmuting the `execute` function pointer to hide
    its argument type, we can instead pass the opaque `*const ()` and let
    the callee cast the pointer type.
    
    Where `HeapJob` was transmuting its own `Box<Self>`, we can instead use
    `Box::into_raw` and `Box::from_raw`. It can also call its function right
    out of the recovered `Box`.

--- rayon-core/src/spawn/mod.rs
+++ rayon-core/src/spawn/mod.rs
@@ -103,7 +103,7 @@ where
             registry.terminate(); // (*) permit registry to terminate now
         }
     }))
-    .as_job_ref()
+    .into_job_ref()
 }
 
 /// Fires off a task into the Rayon threadpool in the "static" or
