2d62dc6b89895fb28210acf0e160669654b970d1
Josh Stone <cuviper@gmail.com>
2022-07-18 14:07:53 -0700

    Assume we have const generics

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 fn main() {
     let ac = autocfg::new();
-    if ac.probe_expression("{ fn _foo<const N: usize>() {} }") {
-        autocfg::emit("has_min_const_generics");
-    }
     if ac.probe_path("std::ops::ControlFlow") {
         autocfg::emit("has_control_flow");
     }
