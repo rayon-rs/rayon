7ffb3c6f16e78c20b7994bf2b627ed86b8b4b6b3
Will Crozier <willcrozi@gmail.com>
2022-09-24 12:50:44 +0100

    cargo fmt

--- tests/clones.rs
+++ tests/clones.rs
@@ -131,7 +131,11 @@ fn clone_adaptors() {
     check(v.par_iter().with_max_len(1).fold(|| 0, |x, _| x));
     check(v.par_iter().with_max_len(1).fold_with(0, |x, _| x));
     check(v.par_iter().with_max_len(1).fold_chunks(1, || 0, |x, _| x));
-    check(v.par_iter().with_max_len(1).fold_chunks_with(1, 0, |x, _| x));
+    check(
+        v.par_iter()
+            .with_max_len(1)
+            .fold_chunks_with(1, 0, |x, _| x),
+    );
     check(v.par_iter().with_max_len(1).try_fold(|| 0, |_, &x| x));
     check(v.par_iter().with_max_len(1).try_fold_with(0, |_, &x| x));
     check(v.par_iter().inspect(|_| ()));
