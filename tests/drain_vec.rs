3e05980c0649603a31520295d67a041dd7a4d276
Josh Stone <cuviper@gmail.com>
2022-11-04 13:03:23 -0700

    Apply suggestions from code review

--- tests/drain_vec.rs
+++ tests/drain_vec.rs
@@ -4,7 +4,7 @@ use rayon::prelude::*;
 fn drain_vec_yielded() {
     let mut vec_org = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    let yielded = vec_org.par_drain(0..5).into_par_iter().collect::<Vec<_>>();
+    let yielded = vec_org.par_drain(0..5).collect::<Vec<_>>();
 
     assert_eq!(&yielded, &[0, 1, 2, 3, 4]);
     assert_eq!(&vec_org, &[5, 6, 7, 8, 9]);
@@ -24,7 +24,7 @@ fn drain_vec_dropped() {
 fn drain_vec_empty_range_yielded() {
     let mut vec_org = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    let yielded = vec_org.par_drain(5..5).into_par_iter().collect::<Vec<_>>();
+    let yielded = vec_org.par_drain(5..5).collect::<Vec<_>>();
 
     assert_eq!(&yielded, &[]);
     assert_eq!(&vec_org, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
