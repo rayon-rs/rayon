035d32307b6b7ee60f9fe345bb7bed815c51589f
Josh Stone <cuviper@gmail.com>
2026-04-15 10:59:57 -0700

    Fix `clippy::useless_conversion`

--- src/iter/walk_tree.rs
+++ src/iter/walk_tree.rs
@@ -234,8 +234,7 @@ where
         // explore while front is of size one.
         while self.to_explore.len() == 1 {
             let front_node = self.to_explore.pop().unwrap();
-            self.to_explore
-                .extend((self.children_of)(&front_node).into_iter());
+            self.to_explore.extend((self.children_of)(&front_node));
             self.seen.push(front_node);
         }
         // now take half of the front.
